08fa0dd80004d94f569ea3f23e4f70609cb4b3ef
KimHyunJin <khj745700@naver.com>
2024-03-06 21:15:56 +0900

    feat: baseAuditing 기능 추가

---
+++ src/main/java/p/khj745700/coucoupang/application/domain/common/CommonEntity.java
@@ -0,0 +1,24 @@
+package p.khj745700.coucoupang.application.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class CommonEntity {
+    @Column
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
